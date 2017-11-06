3806edc7df7a835aa447b826bccc2b8987a4f285
Harish V <vharish85@users.noreply.github.com>
2017-11-06 23:43:13 +0530

    Update filters.py

--- filters.py
+++ filters.py
@@ -3,13 +3,12 @@ from scipy.signal import butter, lfilter
 
 def butter_pass(cutoff, fs, type, order=5):
     nyq = 0.5 * fs
-    freq = cutoff / nyq    
-    b, a = butter(order, freq , btype=type)
+    normal_cutoff = cutoff / nyq
+    b, a = butter(order, normal_cutoff, btype=type, analog=False)
     return b, a
 
-
 def butter_filter(data, cutoff, fs, type, order=5):
-    b, a = butter_pass(cutoff, fs, type, order=order)
+    b, a = butter_pass(cutoff, fs, type,order=order)
     y = lfilter(b, a, data)
     return y
 
